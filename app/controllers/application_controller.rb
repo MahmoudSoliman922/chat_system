1cc168d9c328a0cfce93bca63162cf2c183cad82
Mahmoud <mahmoudsoliman922@gmail.com>
2019-08-02 22:38:39 +0200

    added controller and modified application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
-class ApplicationController < ActionController::Base
+# frozen_string_literal: true
+
+class ApplicationController < ActionController::API
+  # render the response based on success
+  def render_json(result)
+    if result.success?
+      render(json: { success: result.success?, response: result.response, errors: result.errors })
+    else
+      render(json: { success: result.success?, response: result.response, errors: result.errors }, status: 405)
+    end
+  end
 end
