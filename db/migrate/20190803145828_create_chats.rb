c8e99dd11379835334b9acee9bb2490dca4c9874
Mahmoud <mahmoudsoliman922@gmail.com>
2019-08-07 02:24:37 +0200

    added indexes for needed columns

--- db/migrate/20190803145828_create_chats.rb
+++ db/migrate/20190803145828_create_chats.rb
@@ -3,9 +3,9 @@
 class CreateChats < ActiveRecord::Migration[5.2]
   def change
     create_table :chats do |t|
-      t.belongs_to :application, type: :bigint, foreign_key: true
+      t.belongs_to :application, type: :bigint, index: true, foreign_key: true
       t.integer    :messages_count
-      t.integer    :number
+      t.integer    :number, index: true
       t.timestamps
     end
   end
